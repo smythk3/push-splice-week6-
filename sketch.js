3444e6ac65c0820423a6f27b299f14e0806f291c
smythk3 <smythk3@tcd.ie>
2016-11-03 15:41:59 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var nums = [1, 2, 3, 6];
+
+function setup() {
+
+nums.push(63);
+
+println(nums);
+
+nums.splice(4, 1); 
+// dot (.) for "special" features
+
+println(nums);
+
+}
+
+
+
+function draw() {
+  
+}
